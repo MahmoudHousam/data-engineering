c5d104b59e3c1ed2e6e45f902579c5b3b603abc1
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-03-19 20:16:20 +0200

    module 1 - containerization

---
+++ Module_1/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import argparse
+import pyarrow.parquet as pq
+from time import time
+from sqlalchemy import create_engine
+from urllib.parse import urlparse
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+    parsed_url = urlparse(url)
+    file_name = os.path.basename(parsed_url.path)
+    url2 = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
+    os.system(f"curl {url2} -o yellow_tripdata_2023-01.parquet")
+    parquet_file = pq.ParquetFile("yellow_tripdata_2023-01.parquet")
+    for batch in parquet_file.iter_batches(batch_size=100000):
+        start_time = time()
+        print("Fetching a new batch...")
+        batch_df = batch.to_pandas(date_as_object=False)
+        print("Fetched!")
+        print("Appending batch to DB...")
+        batch_df.to_sql(name=table_name, con=engine, if_exists="append")
+        print("Appended!")
+        end_time = time()
+        print(f"Batch took: {end_time - start_time} seconds")
+    
+def parsing_args():
+    parser = argparse.ArgumentParser(description="Ingest NY Taxi data to Postgres DB")
+    parser.add_argument("--user", help="postgres username")
+    parser.add_argument("--password", help="postgres password")
+    parser.add_argument("--host", help="postgres host")
+    parser.add_argument("--port", help="postgres port")
+    parser.add_argument("--db", help="postgres DB name")
+    parser.add_argument("--table_name", help="ingested table")
+    parser.add_argument("--url", help="URL to ingest data from")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parsing_args()
+    main(params=args)
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,43 @@
+# Practice Makes Perfect - Data Engineering
+
+This repository is dedicated to honing skills in data engineering through practical projects and exercises. The objective behind this repository is to provide hands-on experience with various technologies and tools commonly used in the field of data engineering.
+
+## Objective
+
+The main objective of this repository is to develop proficiency in data engineering by working on real-world projects and exercises. By leveraging Python and SQL programming languages, along with a diverse set of technologies and tools, this repository aims to provide comprehensive learning opportunities in data engineering concepts and practices.
+
+## Tech Stack
+
+### Programming Languages
+
+- Python
+- SQL
+
+### Technologies and Tools
+
+- Docker
+- Terraform
+- PostgreSQL
+- Apache Spark (Python & SQL)
+- Google Cloud Platform (GCP)
+- Mage
+- BigQuery
+- DBT (Data Build Tool)
+- Kafka
+- KSQL
+- ksqlDB
+- Make
+
+## Modules
+
+- Module 1: Containerization and Infrastructure as Code (IaC)
+- Module 2: Workflow Orchestration
+- Module 3: Data Warehouse
+- Module 4: Analytics engineering
+- Module 5: Batch processing
+- Module 6: Streaming
+
+## Workshops
+
+- Workshop 1: Data Ingestion
+- Workshop 2: Stream Processing with SQL
