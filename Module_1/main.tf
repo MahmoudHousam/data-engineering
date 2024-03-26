39eab9c7211b2f8345ea2d9bf0d395c53262befa
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-03-26 05:51:35 +0200

    Terraform infra setup with GCP

--- Module_1/main.tf
+++ Module_1/main.tf
@@ -12,3 +12,18 @@ provider "google" {
   project = "resounding-node-418001"
   region  = "us-central1"
 }
+
+resource "google_storage_bucket" "de-cloud-practice" {
+  name          = "node-418001-terra-practice"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
