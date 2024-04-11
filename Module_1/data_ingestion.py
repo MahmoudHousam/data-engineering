41950c701760f4d85d903fa6073f1ec5e45f25e3
MahmoudHousam <mahmoudhousam60@gmail.com>
2024-04-11 05:05:38 +0200

    Pull ICU synthesized clinical data to DB

--- Module_1/data_ingestion.py
+++ Module_1/data_ingestion.py
@@ -1,9 +1,10 @@
 import os
 import argparse
-import pyarrow.parquet as pq
+import pandas as pd
 from time import time
-from sqlalchemy import create_engine
+import pyarrow.parquet as pq
 from urllib.parse import urlparse
+from sqlalchemy import create_engine
 
 
 def main(params):
@@ -15,21 +16,27 @@ def main(params):
     table_name = params.table_name
     url = params.url
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
-    parsed_url = urlparse(url)
-    file_name = os.path.basename(parsed_url.path)
-    url2 = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
-    os.system(f"curl {url2} -o yellow_tripdata_2023-01.parquet")
-    parquet_file = pq.ParquetFile("yellow_tripdata_2023-01.parquet")
-    for batch in parquet_file.iter_batches(batch_size=100000):
-        start_time = time()
-        print("Fetching a new batch...")
-        batch_df = batch.to_pandas(date_as_object=False)
-        print("Fetched!")
-        print("Appending batch to DB...")
-        batch_df.to_sql(name=table_name, con=engine, if_exists="append")
-        print("Appended!")
-        end_time = time()
-        print(f"Batch took: {end_time - start_time} seconds")
+    mimic_df = pd.read_csv("mimic3c.csv")
+    print("Pulling data to DB...")
+    start_time = time()
+    mimic_df.to_sql(name=table_name, con=engine, if_exists="append")
+    end_time = time()
+    print(f"Data pulled to DB in: {end_time - start_time} seconds")
+    # parsed_url = urlparse(url)
+    # file_name = os.path.basename(parsed_url.path)
+    # url2 = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
+    # os.system(f"curl {url2} -o yellow_tripdata_2023-01.parquet")
+    # parquet_file = pq.ParquetFile("yellow_tripdata_2023-01.parquet")
+    # for batch in parquet_file.iter_batches(batch_size=100000):
+    #     start_time = time()
+    #     print("Fetching a new batch...")
+    #     batch_df = batch.to_pandas(date_as_object=False)
+    #     print("Fetched!")
+    #     print("Appending batch to DB...")
+    #     batch_df.to_sql(name=table_name, con=engine, if_exists="append")
+    #     print("Appended!")
+    #     end_time = time()
+    #     print(f"Batch took: {end_time - start_time} seconds")
     
 def parsing_args():
     parser = argparse.ArgumentParser(description="Ingest NY Taxi data to Postgres DB")
